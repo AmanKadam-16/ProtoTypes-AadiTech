e80f6fa5419b8404c3fa2ffd8fbc6d5ee2ebb762
Aman Kadam <126617073+AmanKadam-16@users.noreply.github.com>
2024-12-07 05:49:08 +0000

    small-tweak

---
+++ src/components/Data.ts
@@ -0,0 +1,219 @@
+interface LocationDetail {
+    State: string
+    City: string
+    Country: string
+    Pin: string
+    Language: string
+  }
+export const locationDetails: LocationDetail[] = [
+    {
+      State: "California",
+      City: "Los Angeles",
+      Country: "USA",
+      Pin: "90001",
+      Language: "English"
+    },
+    {
+      State: "Texas",
+      City: "Houston",
+      Country: "USA",
+      Pin: "77001",
+      Language: "English"
+    },
+    {
+      State: "New York",
+      City: "New York City",
+      Country: "USA",
+      Pin: "10001",
+      Language: "English"
+    },
+    {
+      State: "Queensland",
+      City: "Brisbane",
+      Country: "Australia",
+      Pin: "4000",
+      Language: "English"
+    },
+    {
+      State: "Maharashtra",
+      City: "Mumbai",
+      Country: "India",
+      Pin: "400001",
+      Language: "Marathi"
+    },
+    {
+      State: "Ontario",
+      City: "Toronto",
+      Country: "Canada",
+      Pin: "M5H 2N2",
+      Language: "English"
+    },
+    {
+      State: "Beijing",
+      City: "Beijing",
+      Country: "China",
+      Pin: "100000",
+      Language: "Mandarin"
+    },
+    {
+      State: "Île-de-France",
+      City: "Paris",
+      Country: "France",
+      Pin: "75001",
+      Language: "French"
+    },
+    {
+      State: "Berlin",
+      City: "Berlin",
+      Country: "Germany",
+      Pin: "10115",
+      Language: "German"
+    },
+    {
+      State: "Tokyo",
+      City: "Tokyo",
+      Country: "Japan",
+      Pin: "100-0001",
+      Language: "Japanese"
+    },
+    {
+      State: "Rio de Janeiro",
+      City: "Rio de Janeiro",
+      Country: "Brazil",
+      Pin: "20000-000",
+      Language: "Portuguese"
+    },
+    {
+      State: "England",
+      City: "London",
+      Country: "United Kingdom",
+      Pin: "EC1A 1BB",
+      Language: "English"
+    },
+    {
+      State: "Gauteng",
+      City: "Johannesburg",
+      Country: "South Africa",
+      Pin: "2000",
+      Language: "Zulu"
+    },
+    {
+      State: "Moscow",
+      City: "Moscow",
+      Country: "Russia",
+      Pin: "101000",
+      Language: "Russian"
+    },
+    {
+      State: "Dubai",
+      City: "Dubai",
+      Country: "United Arab Emirates",
+      Pin: "00000",
+      Language: "Arabic"
+    },
+    {
+      State: "New South Wales",
+      City: "Sydney",
+      Country: "Australia",
+      Pin: "2000",
+      Language: "English"
+    },
+    {
+      State: "Tamil Nadu",
+      City: "Chennai",
+      Country: "India",
+      Pin: "600001",
+      Language: "Tamil"
+    },
+    {
+      State: "Bavaria",
+      City: "Munich",
+      Country: "Germany",
+      Pin: "80331",
+      Language: "German"
+    },
+    {
+      State: "Seoul",
+      City: "Seoul",
+      Country: "South Korea",
+      Pin: "03000",
+      Language: "Korean"
+    },
+    {
+      State: "Lombardy",
+      City: "Milan",
+      Country: "Italy",
+      Pin: "20100",
+      Language: "Italian"
+    },
+    {
+      State: "Bali",
+      City: "Denpasar",
+      Country: "Indonesia",
+      Pin: "80232",
+      Language: "Indonesian"
+    },
+    {
+      State: "Buenos Aires",
+      City: "Buenos Aires",
+      Country: "Argentina",
+      Pin: "C1000",
+      Language: "Spanish"
+    },
+    {
+      State: "Madrid",
+      City: "Madrid",
+      Country: "Spain",
+      Pin: "28001",
+      Language: "Spanish"
+    },
+    {
+      State: "Istanbul",
+      City: "Istanbul",
+      Country: "Turkey",
+      Pin: "34000",
+      Language: "Turkish"
+    },
+    {
+      State: "Bangkok",
+      City: "Bangkok",
+      Country: "Thailand",
+      Pin: "10100",
+      Language: "Thai"
+    },
+    {
+      State: "Victoria",
+      City: "Melbourne",
+      Country: "Australia",
+      Pin: "3000",
+      Language: "English"
+    },
+    {
+      State: "Quebec",
+      City: "Montreal",
+      Country: "Canada",
+      Pin: "H3A 1E4",
+      Language: "French"
+    },
+    {
+      State: "West Bengal",
+      City: "Kolkata",
+      Country: "India",
+      Pin: "700001",
+      Language: "Bengali"
+    },
+    {
+      State: "Texas",
+      City: "Austin",
+      Country: "USA",
+      Pin: "73301",
+      Language: "English"
+    },
+    {
+      State: "Nevada",
+      City: "Las Vegas",
+      Country: "USA",
+      Pin: "89101",
+      Language: "English"
+    }
+  ];
